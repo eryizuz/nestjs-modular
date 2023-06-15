6e474a1b4fec7a0bca6ed103aa92e58a5eac35e0
jesus lopez <jjezzlopezz@hotmail.com>
2023-06-15 09:47:03 -0400

    docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  // Configuración Swagger en NestJS
+  const config = new DocumentBuilder()
+    .setTitle('Platzi API')
+    .setDescription('Documentación Platzi API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  // URL API
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
